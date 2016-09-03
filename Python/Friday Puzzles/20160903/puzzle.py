2926345afacfe1a7b05c9c35d7f10121d44460a4
Mingo Hagen <email@mingo.nl>
2016-09-03 23:47:45 +0200

    Bit of cleanup and assert on error message content

--- Python/Friday Puzzles/20160903/puzzle.py
+++ Python/Friday Puzzles/20160903/puzzle.py
@@ -1,15 +1,16 @@
-# Main function:
+import unittest
+
 class ByteConverter:
   def __init__(self):
     self.oneKiB=2**10
-    self.prefixes=['','Ki','Mi','Gi','Ti','Pi','Ei']
+    self.prefixes=['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei']
 
-  def toHumanReadable(self,numberOfBytes):
+  def toHumanReadable(self, numberOfBytes):
     # error checking:
     try:
-       a = int(numberOfBytes)
+      int(numberOfBytes)
     except ValueError:
-       raise ValueError('Input must be a number')
+      raise ValueError('Input must be a number')
 
     if numberOfBytes <= 0:
       raise ValueError('Input must be greater than 0')
@@ -20,32 +21,34 @@ class ByteConverter:
     # the actual conversion:
     for prefix in self.prefixes:
       if numberOfBytes < self.oneKiB:
-        return '%i %sB' % (numberOfBytes,prefix)
+        return '%i %sB' % (numberOfBytes, prefix)
       numberOfBytes /= self.oneKiB
 
-# Unit Tests Below:
-import unittest
 
+
+
+
+# Unit Tests Below:
 class Test_ByteConverter(unittest.TestCase):
   def setUp(self):
     self.byteConverter = ByteConverter()
 
   def test_one_byte(self):
-    self.assertEqual(self.byteConverter.toHumanReadable(1),'1 B')
+    self.assertEqual(self.byteConverter.toHumanReadable(1), '1 B')
   def test_one_kibibyte(self):
-    self.assertEqual(self.byteConverter.toHumanReadable(1024),'1 KiB')
+    self.assertEqual(self.byteConverter.toHumanReadable(1024), '1 KiB')
   def test_one_mebibyte(self):
-    self.assertEqual(self.byteConverter.toHumanReadable(1048576),'1 MiB')
+    self.assertEqual(self.byteConverter.toHumanReadable(1048576), '1 MiB')
   def test_another_mebibyte(self):
-    self.assertEqual(self.byteConverter.toHumanReadable(1234567),'1 MiB')
+    self.assertEqual(self.byteConverter.toHumanReadable(1234567), '1 MiB')
   def test_nine_gibibytes(self):
-    self.assertEqual(self.byteConverter.toHumanReadable(9999999999),'9 GiB')
+    self.assertEqual(self.byteConverter.toHumanReadable(9999999999), '9 GiB')
   def test_cellar(self):
-    self.assertRaises(ValueError,self.byteConverter.toHumanReadable,-5)
+    self.assertRaisesRegex(ValueError, 'Input must be greater than 0', self.byteConverter.toHumanReadable, -5)
   def test_attic(self):
-    self.assertRaises(ValueError,self.byteConverter.toHumanReadable,2**63)
+    self.assertRaisesRegex(ValueError, 'Input must be no larger than 2\^63-1', self.byteConverter.toHumanReadable, 2**63)
   def test_nan(self):
-    self.assertRaises(ValueError,self.byteConverter.toHumanReadable,'I am not a number.')
+    self.assertRaisesRegex(ValueError, 'Input must be a number', self.byteConverter.toHumanReadable, 'I am not a number.')
 
 if __name__ == '__main__':
     unittest.main()
